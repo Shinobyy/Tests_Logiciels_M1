d9e8a5c58d03d1ebc8dfad37d5f3ff70afcb1f49
Shinobyy <corentin.casset@gmail.com>
2025-10-31 16:13:32 +0100

    Add Officine class with tests and project setup

---
+++ main.js
@@ -0,0 +1,102 @@
+class Officine {
+  constructor() {
+    this.ingredients = [
+      "œil/yeux de grenouille",
+      "larme de brume funèbre",
+      "radicelle de racine hurlante",
+      "pincée de poudre de lune",
+      "croc de troll",
+      "fragment d'écaille de dragonnet",
+      "goutte de sang de Citrouille",
+    ];
+    this.recettes = {
+      "fiole de glaires purulentes": [
+        "2 larmes de brume funèbre",
+        "1 goutte de sang de Citrouille",
+      ],
+      "bille d'âme évanescente": [
+        "3 pincées de poudre de lune",
+        "1 œil de grenouille",
+      ],
+      "soupçon de sels suffocants": [
+        "2 crocs de troll",
+        "1 fragment d'écaille de dragonnet",
+        "1 radicelle de racine hurlante",
+      ],
+      "baton de pâte sépulcrale": [
+        "3 radicelles de racine hurlante",
+        "1 fiole de glaires purulentes",
+      ],
+      "bouffée d'essence de cauchemar": [
+        "2 pincées de poudre de lune",
+        "2 larmes de brume funèbre",
+      ],
+    };
+    this.singularMap = {
+      "yeux de grenouille": "œil de grenouille",
+      "larmes de brume funèbre": "larme de brume funèbre",
+      "radicelles de racine hurlante": "radicelle de racine hurlante",
+      "pincées de poudre de lune": "pincée de poudre de lune",
+      "crocs de troll": "croc de troll",
+      "fragments d'écaille de dragonnet": "fragment d'écaille de dragonnet",
+      "gouttes de sang de Citrouille": "goutte de sang de Citrouille",
+      "fioles de glaires purulentes": "fiole de glaires purulentes",
+      "billes d'âme évanescente": "bille d'âme évanescente",
+      "soupçons de sels suffocants": "soupçon de sels suffocants",
+      "batons de pâte sépulcrale": "baton de pâte sépulcrale",
+      "bouffées d'essence de cauchemar": "bouffée d'essence de cauchemar",
+    };
+    this.stocks = new Map();
+  }
+
+  singularize(item) {
+    return this.singularMap[item] || item;
+  }
+
+  rentrer(str) {
+    const match = str.match(/^(\d+)\s+(.+)$/);
+    if (!match) return;
+    const qty = parseInt(match[1], 10);
+    const item = match[2];
+    const sing = this.singularize(item);
+    this.stocks.set(sing, (this.stocks.get(sing) || 0) + qty);
+  }
+
+  quantite(item) {
+    const sing = this.singularize(item);
+    return this.stocks.get(sing) || 0;
+  }
+
+  preparer(str) {
+    const match = str.match(/^(\d+)\s+(.+)$/);
+    if (!match) return 0;
+    const requested = parseInt(match[1], 10);
+    const item = match[2];
+    const sing = this.singularize(item);
+    const recipe = this.recettes[sing];
+    if (!recipe) return 0;
+    let maxPossible = Infinity;
+    const requirements = [];
+    for (const reqStr of recipe) {
+      const m = reqStr.match(/^(\d+)\s+(.+)$/);
+      if (!m) continue;
+      const reqQty = parseInt(m[1], 10);
+      const reqItem = m[2];
+      const reqSing = this.singularize(reqItem);
+      const available = this.stocks.get(reqSing) || 0;
+      const canMake = Math.floor(available / reqQty);
+      if (canMake < maxPossible) maxPossible = canMake;
+      requirements.push({ reqSing, reqQty });
+    }
+    const actual = Math.min(requested, maxPossible);
+    if (actual > 0) {
+      for (const { reqSing, reqQty } of requirements) {
+        this.stocks.set(reqSing, this.stocks.get(reqSing) - actual * reqQty);
+      }
+      this.stocks.set(sing, (this.stocks.get(sing) || 0) + actual);
+    }
+    return actual;
+  }
+}
+
+module.exports = Officine;
---
+++ tests/main.test.js
@@ -0,0 +1,139 @@
+const Officine = require("../main");
+
+test("rentrer_cas_usuel_ingredient_connu_pluriel", () => {
+  const officine = new Officine();
+  officine.rentrer("3 yeux de grenouille");
+  expect(officine.quantite("œil de grenouille")).toBe(3);
+  expect(officine.quantite("yeux de grenouille")).toBe(3);
+});
+
+test("rentrer_cas_usuel_ingredient_connu_singulier", () => {
+  const officine = new Officine();
+  officine.rentrer("3 œil de grenouille");
+  expect(officine.quantite("œil de grenouille")).toBe(3);
+  expect(officine.quantite("yeux de grenouille")).toBe(3);
+});
+
+test("rentrer_cas_extreme_quantite_grande", () => {
+  const officine = new Officine();
+  officine.rentrer("999999999 larmes de brume funèbre");
+  expect(officine.quantite("larmes de brume funèbre")).toBe(999999999);
+  expect(officine.quantite("larme de brume funèbre")).toBe(999999999);
+});
+
+test("rentrer_cas_extreme_quantite_zero", () => {
+  const officine = new Officine();
+  officine.rentrer("0 crocs de troll");
+  expect(officine.quantite("croc de troll")).toBe(0);
+});
+
+test("rentrer_cas_erreur_chaine_invalide", () => {
+  const officine = new Officine();
+  officine.rentrer("trois yeux de grenouille");
+  expect(officine.quantite("œil de grenouille")).toBe(0);
+});
+
+test("rentrer_cas_erreur_quantite_negative", () => {
+  const officine = new Officine();
+  officine.rentrer("-2 pincées de poudre de lune");
+  expect(officine.quantite("pincée de poudre de lune")).toBe(0);
+});
+
+test("rentrer_cas_erreur_ingredient_inconnu", () => {
+  const officine = new Officine();
+  officine.rentrer("3 poils de licorne");
+  expect(officine.quantite("poil de licorne")).toBe(0);
+});
+
+test("quantite_cas_usuel_singulier_en_stock", () => {
+  const officine = new Officine();
+  officine.rentrer("4 crocs de troll");
+  expect(officine.quantite("croc de troll")).toBe(4);
+});
+
+test("quantite_cas_usuel_pluriel_en_stock", () => {
+  const officine = new Officine();
+  officine.rentrer("2 fragments d'écaille de dragonnet");
+  expect(officine.quantite("fragments d'écaille de dragonnet")).toBe(2);
+});
+
+test("quantite_cas_usuel_non_en_stock", () => {
+  const officine = new Officine();
+  expect(officine.quantite("radicelle de racine hurlante")).toBe(0);
+});
+
+test("quantite_cas_extreme_apres_operations_multiples", () => {
+  const officine = new Officine();
+  officine.rentrer("2 crocs de troll");
+  officine.rentrer("3 crocs de troll");
+  officine.rentrer("5 crocs de troll");
+  expect(officine.quantite("croc de troll")).toBe(10);
+});
+
+test("quantite_cas_erreur_ingredient_inconnu", () => {
+  const officine = new Officine();
+  expect(officine.quantite("bave de lutin")).toBe(0);
+});
+
+test("preparer_cas_usuel_stocks_suffisants", () => {
+  const officine = new Officine();
+  officine.rentrer("5 larmes de brume funèbre");
+  officine.rentrer("3 gouttes de sang de Citrouille");
+  const nb = officine.preparer("2 fioles de glaires purulentes");
+  expect(nb).toBe(2);
+  expect(officine.quantite("fiole de glaires purulentes")).toBe(2);
+  expect(officine.quantite("larme de brume funèbre")).toBe(1);
+  expect(officine.quantite("goutte de sang de Citrouille")).toBe(1);
+});
+
+test("preparer_cas_usuel_stocks_insuffisants", () => {
+  const officine = new Officine();
+  officine.rentrer("1 larme de brume funèbre");
+  officine.rentrer("3 gouttes de sang de Citrouille");
+  const nb = officine.preparer("3 fioles de glaires purulentes");
+  expect(nb).toBe(0);
+  expect(officine.quantite("fiole de glaires purulentes")).toBe(0);
+});
+
+test("preparer_cas_usuel_potion_utilisant_potion", () => {
+  const officine = new Officine();
+  officine.rentrer("6 radicelles de racine hurlante");
+  officine.rentrer("2 larmes de brume funèbre");
+  officine.rentrer("1 goutte de sang de Citrouille");
+
+  officine.preparer("1 fiole de glaires purulentes");
+  officine.preparer("1 baton de pâte sépulcrale");
+
+  expect(officine.quantite("baton de pâte sépulcrale")).toBe(1);
+  expect(officine.quantite("radicelle de racine hurlante")).toBe(3);
+  expect(officine.quantite("fiole de glaires purulentes")).toBe(0);
+});
+
+test("preparer_cas_extreme_quantite_grande", () => {
+  const officine = new Officine();
+  officine.rentrer("1000 larmes de brume funèbre");
+  officine.rentrer("500 gouttes de sang de Citrouille");
+  const nb = officine.preparer("300 fioles de glaires purulentes");
+  expect(nb).toBe(300);
+  expect(officine.quantite("fiole de glaires purulentes")).toBe(300);
+});
+
+test("preparer_cas_extreme_quantite_zero", () => {
+  const officine = new Officine();
+  const nb = officine.preparer("0 fioles de glaires purulentes");
+  expect(nb).toBe(0);
+});
+
+test("preparer_cas_erreur_potion_inconnue", () => {
+  const officine = new Officine();
+  officine.rentrer("10 radicelles de racine hurlante");
+  const nb = officine.preparer("1 potion de lévitation");
+  expect(nb).toBe(0);
+});
+
+test("preparer_cas_erreur_chaine_invalide", () => {
+  const officine = new Officine();
+  officine.rentrer("10 larmes de brume funèbre");
+  const nb = officine.preparer("une fiole de glaires purulentes");
+  expect(nb).toBe(0);
+});
